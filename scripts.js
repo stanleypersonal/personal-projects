42420e6f47d9f90cb51d8006e035c273e4cbbe8d
stanleypersonal <stanleyndonj@gmail.com>
2024-06-24 21:26:24 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,14 @@
+// Toggle Navigation Menu
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+    burger.classList.toggle('toggle');
+});
+
+// Sticky Header on Scroll
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 0);
+});
